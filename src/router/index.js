49e27d8ec20134f055c2b420486099d5108fc278
CamilaBaron2 <camiilaapaez12@gmail.com>
2022-11-12 23:19:51 -0500

    create docs

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 import PrincipalView from '../views/PrincipalView.vue'
 
 const routes = [
   {
-    path: '/',
+    path: './',
     name: 'principal',
     component: PrincipalView
   },
   {
-    path: '/sistemas-operativos',
+    path: './sistemas-operativos',
     name: 'sistemas-operativos',
     component: () => import('../views/SistemasOperativosView.vue')
   },
   {
-    path: '/sistema-operativo/:id',
+    path: './sistema-operativo/:id',
     name: 'sistema-operativo',
     component: () => import('../views/SistemaOperativoView.vue')
   },
   {
-    path: '/hardware',
+    path: './hardware',
     name:'hardware',
     component: () => import ('../views/HardwareView.vue')
   },
   {
-    path: '/hardware2/:id',
+    path: './hardware2/:id',
     name:'hardware2',
     component: () => import ('../views/Hardware2View.vue')
   },
   {
-    path: '/glosario',
+    path: './glosario',
     name:'glosario',
     component: () => import ('../views/GlosarioView.vue')
   },
   {
-    path: '/ingles',
+    path: './ingles',
     name: '/ingles',
     component: () => import('../views/InglesView.vue')
   }
@@ -41,7 +41,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
